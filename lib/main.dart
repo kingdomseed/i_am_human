54b0e5d03851eff74b3366b850a61e9d1d567814
Jason M Holt <kingdomseed@gmail.com>
2020-05-15 19:05:09 -0500

    Finished setting up basics in app.

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,16 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
         home: Scaffold(
-      backgroundColor: Colors.blueAccent,
+      backgroundColor: Colors.lightBlueAccent,
       appBar: AppBar(
         title: Text(
           'I am Human',
         ),
         backgroundColor: Colors.blue,
       ),
-      body: Center(child: Image(image: AssetImage(''))),
+      body: Center(
+          child: Image(
+        image: AssetImage('images/ThreeParts.png'),
+        height: 300,
+      )),
     )));
